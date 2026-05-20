eb0dde66dd329511466de1a47b81b099935cd862
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2026-05-20 11:34:59 -0300

    mais mundanças

--- Quiz/js/geografia.js
+++ Quiz/js/geografia.js
@@ -6,14 +6,16 @@ function conferir1() {
   let resposta = document.getElementById('r1').value;
   if (resposta == 'c' || resposta == 'C') {
     document.getElementById('ce1').innerHTML = 'Resposta certa :)';
-    document.getElementById('d1').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d1').style.backgroundColor = '#93db93';
+    document.getElementById('r1').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce1').innerHTML = 'Resposta errada :(';
-    document.getElementById('d1').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d1').style.backgroundColor = '#d17979';
+    document.getElementById('r1').style.display = 'none';
   }
   document.getElementById('c1').style.display = 'none';
-  document.getElementById('proximo1').style.display = 'inline-block';
+  document.getElementById('proximo1').style.display = 'block';
 }
 
 //pergunta 2
@@ -21,14 +23,16 @@ function conferir2() {
   let resposta = document.getElementById('r2').value;
   if (resposta == 'a' || resposta == 'A') {
     document.getElementById('ce2').innerHTML = 'Resposta certa :)';
-    document.getElementById('d2').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d2').style.backgroundColor = '#93db93';
+    document.getElementById('r2').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce2').innerHTML = 'Resposta errada :(';
-    document.getElementById('d2').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d2').style.backgroundColor = '#d17979';
+    document.getElementById('r2').style.display = 'none';
   }
   document.getElementById('c2').style.display = 'none';
-  document.getElementById('proximo2').style.display = 'inline-block';
+  document.getElementById('proximo2').style.display = 'block';
 }
 
 //pergunta3
@@ -36,14 +40,16 @@ function conferir3() {
   let resposta = document.getElementById('r3').value;
   if (resposta == 'b' || resposta == 'B') {
     document.getElementById('ce3').innerHTML = 'Resposta certa :)';
-    document.getElementById('d3').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d3').style.backgroundColor = '#93db93';
+    document.getElementById('r3').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce3').innerHTML = 'Resposta errada :(';
-    document.getElementById('d3').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d3').style.backgroundColor = '#d17979';
+    document.getElementById('r3').style.display = 'none';
   }
   document.getElementById('c3').style.display = 'none';
-  document.getElementById('proximo3').style.display = 'inline-block';
+  document.getElementById('proximo3').style.display = 'block';
 }
 
 //pergunta4
@@ -51,14 +57,16 @@ function conferir4() {
   let resposta = document.getElementById('r4').value;
   if (resposta == 'c' || resposta == 'C') {
     document.getElementById('ce4').innerHTML = 'Resposta certa :)';
-    document.getElementById('d4').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d4').style.backgroundColor = '#93db93';
+    document.getElementById('r4').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce4').innerHTML = 'Resposta errada :(';
-    document.getElementById('d4').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d4').style.backgroundColor = '#d17979';
+    document.getElementById('r4').style.display = 'none';
   }
   document.getElementById('c4').style.display = 'none';
-  document.getElementById('proximo4').style.display = 'inline-block';
+  document.getElementById('proximo4').style.display = 'block';
 }
 
 //pergunta5
@@ -66,14 +74,16 @@ function conferir5() {
   let resposta = document.getElementById('r5').value;
   if (resposta == 'c' || resposta == 'C') {
     document.getElementById('ce5').innerHTML = 'Resposta certa :)';
-    document.getElementById('d5').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d5').style.backgroundColor = '#93db93';
+    document.getElementById('r5').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce5').innerHTML = 'Resposta errada :(';
-    document.getElementById('d5').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d5').style.backgroundColor = '#d17979';
+    document.getElementById('r5').style.display = 'none';
   }
   document.getElementById('c5').style.display = 'none';
-  document.getElementById('proximo5').style.display = 'inline-block';
+  document.getElementById('proximo5').style.display = 'block';
 }
 
 //pergunta6
@@ -81,14 +91,16 @@ function conferir6() {
   let resposta = document.getElementById('r6').value;
   if (resposta == 'd' || resposta == 'D') {
     document.getElementById('ce6').innerHTML = 'Resposta certa :)';
-    document.getElementById('d6').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d6').style.backgroundColor = '#93db93';
+    document.getElementById('r6').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce6').innerHTML = 'Resposta errada :(';
-    document.getElementById('d6').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d6').style.backgroundColor = '#d17979';
+    document.getElementById('r6').style.display = 'none';
   }
   document.getElementById('c6').style.display = 'none';
-  document.getElementById('proximo6').style.display = 'inline-block';
+  document.getElementById('proximo6').style.display = 'block';
 }
 
 //pergunta7
@@ -96,14 +108,16 @@ function conferir7() {
   let resposta = document.getElementById('r7').value;
   if (resposta == 'a' || resposta == 'A') {
     document.getElementById('ce7').innerHTML = 'Resposta certa :)';
-    document.getElementById('d7').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d7').style.backgroundColor = '#93db93';
+    document.getElementById('r7').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce7').innerHTML = 'Resposta errada :(';
-    document.getElementById('d7').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d7').style.backgroundColor = '#d17979';
+    document.getElementById('r7').style.display = 'none';
   }
   document.getElementById('c7').style.display = 'none';
-  document.getElementById('proximo7').style.display = 'inline-block';
+  document.getElementById('proximo7').style.display = 'block';
 }
 
 //pergunta8
@@ -111,14 +125,16 @@ function conferir8() {
   let resposta = document.getElementById('r8').value;
   if (resposta == 'c' || resposta == 'C') {
     document.getElementById('ce8').innerHTML = 'Resposta certa :)';
-    document.getElementById('d8').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d8').style.backgroundColor = '#93db93';
+    document.getElementById('r8').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce8').innerHTML = 'Resposta errada :(';
-    document.getElementById('d8').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d8').style.backgroundColor = '#d17979';
+    document.getElementById('r8').style.display = 'none';
   }
   document.getElementById('c8').style.display = 'none';
-  document.getElementById('proximo8').style.display = 'inline-block';
+  document.getElementById('proximo8').style.display = 'block';
 }
 
 //pergunta9
@@ -126,14 +142,16 @@ function conferir9() {
   let resposta = document.getElementById('r9').value;
   if (resposta == 'b' || resposta == 'B') {
     document.getElementById('ce9').innerHTML = 'Resposta certa :)';
-    document.getElementById('d9').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d9').style.backgroundColor = '#93db93';
+    document.getElementById('r9').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce9').innerHTML = 'Resposta errada :(';
-    document.getElementById('d9').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d9').style.backgroundColor = '#d17979';
+    document.getElementById('r9').style.display = 'none';
   }
   document.getElementById('c9').style.display = 'none';
-  document.getElementById('proximo9').style.display = 'inline-block';
+  document.getElementById('proximo9').style.display = 'block';
 }
 
 //pergunta10
@@ -141,14 +159,16 @@ function conferir10() {
   let resposta = document.getElementById('r10').value;
   if (resposta == 'b' || resposta == 'B') {
     document.getElementById('ce10').innerHTML = 'Resposta certa :)';
-    document.getElementById('d10').style.backgroundColor = '#ccffcc';
+    // document.getElementById('d10').style.backgroundColor = '#93db93';
+    document.getElementById('r10').style.display = 'none';
     ponto = ponto + 1;
   } else {
     document.getElementById('ce10').innerHTML = 'Resposta errada :(';
-    document.getElementById('d10').style.backgroundColor = '#ffcbcb';
+    // document.getElementById('d10').style.backgroundColor = '#d17979';
+    document.getElementById('r10').style.display = 'none';
   }
   document.getElementById('c10').style.display = 'none';
-  document.getElementById('proximo10').style.display = 'inline-block';
+  document.getElementById('proximo10').style.display = 'block';
 }
 
 //ir para a proxima pergunta
